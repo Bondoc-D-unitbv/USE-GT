ed7901621bb3e319649970ba14c67975c5918da6
Daniel Bondoc <daniel.bondoc@student.unitbv.ro>
2025-11-30 21:26:39 +0200

    add Student

--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -1,4 +1,25 @@
 package org.example.model;
 
+import java.util.ArrayList;
+
 public class Student {
+    private Integer studentId;
+    private String studentName;
+    private ArrayList<Course> coursesEnrolled;
+
+    public Student(Integer studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.coursesEnrolled = new ArrayList<>();
+    }
+
+    // Getters and Setters
+    public Integer getStudentId() { return studentId; }
+    public void setStudentId(Integer studentId) { this.studentId = studentId; }
+
+    public String getStudentName() { return studentName; }
+    public void setStudentName(String studentName) { this.studentName = studentName; }
+
+    public ArrayList<Course> getCoursesEnrolled() { return coursesEnrolled; }
+    public void setCoursesEnrolled(ArrayList<Course> coursesEnrolled) { this.coursesEnrolled = coursesEnrolled; }
 }
